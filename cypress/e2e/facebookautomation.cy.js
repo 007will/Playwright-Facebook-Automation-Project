39787e0d1e7dc43bd09c95a234611471c6013bfd
007Will <renome@live.com>
2025-10-19 00:47:38 -0300

    Login e Senha - OK - Falta Mobile mode Playright

--- cypress/e2e/facebookautomation.cy.js
+++ cypress/e2e/facebookautomation.cy.js
@@ -2,5 +2,13 @@ describe('Facebook Automation', () => {
   it('Automacao', () => {
     cy.visit('/', { failOnStatusCode: false })
     cy.get('#icon').click()
+    cy.get('[data-testid="royal-email"]').type('willams@outlook.com.br')
+    cy.get('[data-testid="royal-pass"]').type('t5T^%PL@J@gA*MeQm%5E4G))@F')
+    cy.wait(3000)
+    cy.get('[data-testid="royal-login-button"]').click()
+
+
+
   })
 })
+
